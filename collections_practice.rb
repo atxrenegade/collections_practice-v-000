a3e2010f7c1ad68212d6b38c14c850b393dc7a18
Harleigh Abel <harleighabel@gmail.com>
2018-02-03 00:20:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,65 +1,79 @@
-
 def sort_array_asc(array)
-  array.sort
+  array.sort!
 end
 
 def sort_array_desc(array)
-  array.sort {|a, b| b <=> a}
-end
-
-def sort_array_char_count(array)
-  array.sort! do |a, b|
-    a.length <=> b.length
+  array.sort do |a, b|
+  b<=>a
   end
 end
 
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length > b.length
+      b<=>a
+    end  
+   end
+   array
+end  
+
 def swap_elements(array)
-    swap = array[1]
+      swap = array[1]
     array[1] = array[2]
     array[2] = swap
-  array
+    array
 end
-
-=begin
-# another version of #swap_elements
-def swap_elements(array)
-    array[1], array[2] = array[2], array[1]
-  array
-end
-=end
-
-# advanced #swap_elements (to be written)
+  
 
 def reverse_array(array)
   array.reverse!
 end
 
 def kesha_maker(array)
-  array.collect! do |word|
-    word = word.chars.to_a
-    word[2] = "$"
-    word.join
-  end
-end
+  array.map do |word|
+  counter = 0
+    while counter < word.length
+      if counter == 2 
+        word[counter] = "$"
+      else
+        word[counter]
+      end
+    counter += 1
+    end
+  end 
+  return array
+end  
 
-# alternate kesha_maker (using #each, to be written)
 
 def find_a(array)
-  array.find_all { |word|  word.start_with? "a" }
-end
+  a_array = []
+  array.each do |word|
+    if word.start_with?("a") || word.start_with?("A")
+      a_array << word
+    end  
+  end
+  a_array
+end 
+
 
 def sum_array(array)
-  array.inject { |sum, number| sum + number }
+  counter = 0
+  while counter < array.length
+    sum = 0
+    array.each do | n |
+      sum = sum + n
+      counter += 1
+    end 
+  end
+  sum
 end
 
 def add_s(array)
-  array.collect! do |word|
-   if word == array[1]
-     word
-   else
-     "#{word}s"
+  array.each_with_index do |word, index|
+    if index == 1 
+      word
+    else 
+      word << "s"
     end
-  end
+  end  
 end
-
-# alternate #add_s (refactored, to be written)
